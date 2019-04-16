467be2214e06b8932659148594e46d790f8c3be3
moavia13 <48994163+moavia13@users.noreply.github.com>
2019-04-16 10:18:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials')
+var app = express();
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+  return text.toUpperCase();
+});
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res) => {
+  //res.send('Hello Express!');
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my page',
+    currentYear: new Date().getFullYear()
+  });
+});
+app.get('/about', (req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+app.listen(3000);
